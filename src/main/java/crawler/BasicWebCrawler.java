996f92dc8b9fb9f5622511efc2033507a197a836
jorcrack <jsananzam@hotmail.com>
2019-02-28 21:02:39 +0100

    Ejercicio Crawler

--- src/main/java/crawler/BasicWebCrawler.java
+++ src/main/java/crawler/BasicWebCrawler.java
@@ -27,11 +27,12 @@ public class BasicWebCrawler {
                 if (isValid(URL) && URL.startsWith("http")) {
                     links.add(URL);
                     Document document = Jsoup.connect(URL).get();
-                    //TODO
-                    Elements linksOnPage = document.select("");
+                    Elements linksOnPage = document.select("a[href]");
                     depth++;
                     for (Element page : linksOnPage) {
-                        //TODO get all links for every page
+                        String link = page.attr( "href");
+                        getAllLinksFromWebsite(link,depth);
+
                     }
                 }
             } catch (Exception e) {
@@ -46,6 +47,9 @@ public class BasicWebCrawler {
             writer = new FileWriter(filename);
             for(String link : links)
                 try {
+
+                    writer.write("- Url: " +link+ "\n\n");
+
                     //TODO write to file
 
                 } catch (Exception e) {
